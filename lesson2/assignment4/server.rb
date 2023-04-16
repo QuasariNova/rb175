00f40c29e149fc987d84e702a9960d80bcbaf4c1
Christopher Perkins <Christopher.Perkins@null.net>
2023-04-16 09:05:29 -0400

    L2A4: Solved parsing request line.

--- lesson2/assignment4/server.rb
+++ lesson2/assignment4/server.rb
@@ -1,5 +1,18 @@
 require 'socket'
 
+def split(request_line)
+  http_method, path_and_params, _ = request_line.split
+
+  path, param_strs = path_and_params.split '?'
+
+  return [http_method, path, {}] unless param_strs
+
+  param_strs = param_strs.split '&'
+
+  params = param_strs.map { |param| param.split '=' }.to_h
+  [http_method, path, params]
+end
+
 server = TCPServer.new('localhost', 3003)
 loop do
   client = server.accept
@@ -8,9 +21,20 @@ loop do
   next if !request_line || request_line =~ /favicon/
   puts request_line
 
+  http_method, path, params = split(request_line)
+
   client.puts "HTTP/1.1 200 OK"
   client.puts "Content-Type: text/plain\r\n\r\n"
+
   client.puts request_line
-  client.puts rand(6) + 1
+  client.puts http_method
+  client.puts path
+  client.puts params
+
+  sides = params.fetch('sides') { 6 }.to_i
+  rolls = params.fetch('rolls') { 1 }.to_i
+
+  rolls.times { client.puts rand(sides) + 1}
+
   client.close
 end
