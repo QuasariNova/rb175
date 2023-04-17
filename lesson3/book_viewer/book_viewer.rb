41d14a2748bb70de336b0a246822f5150798bbf9
Christopher Perkins <Christopher.Perkins@null.net>
2023-04-17 07:03:51 -0400

    L3A12: before filter

--- lesson3/book_viewer/book_viewer.rb
+++ lesson3/book_viewer/book_viewer.rb
@@ -2,15 +2,16 @@ require "sinatra"
 require "sinatra/reloader"
 require 'tilt/erubis'
 
+before do
+  @contents = File.readlines 'data/toc.txt'
+end
+
 get "/" do
-  @title = "The Adventures of Sherlock Holmes"
   @contents = File.readlines 'data/toc.txt'
   erb :home
 end
 
 get "/chapters/:number" do
-  @contents = File.readlines 'data/toc.txt'
-
   number = params[:number].to_i
   @title = "Chapter #{number}: #{@contents[number - 1]}"
 
