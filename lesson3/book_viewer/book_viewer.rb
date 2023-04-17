b02d6a178960e0f88142bf4d26c05f09f321b21f
Christopher Perkins <Christopher.Perkins@null.net>
2023-04-17 07:00:59 -0400

    L2A11: Route Parameter for chapters

--- lesson3/book_viewer/book_viewer.rb
+++ lesson3/book_viewer/book_viewer.rb
@@ -8,10 +8,13 @@ get "/" do
   erb :home
 end
 
-get "/chapters/1" do
-  @title = 'Chapter 1'
+get "/chapters/:number" do
   @contents = File.readlines 'data/toc.txt'
-  @chapter = File.read 'data/chp1.txt'
+
+  number = params[:number].to_i
+  @title = "Chapter #{number}: #{@contents[number - 1]}"
+
+  @chapter = File.read "data/chp#{number}.txt"
   @chapter = @chapter.split "\n\n"
 
   erb :chapter
