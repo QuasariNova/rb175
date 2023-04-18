40aef8be9690373839473bc945bf8296c102eba3
Christopher Perkins <Christopher.Perkins@null.net>
2023-04-18 07:54:38 -0400

    L5A6: Followed Video Create Lists

--- lesson5/sinatra_todos/todo.rb
+++ lesson5/sinatra_todos/todo.rb
@@ -2,14 +2,29 @@ require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
 
+configure do
+  enable :sessions
+  set :session_secret, 'secretsecretsecretsecretsecretsecretsecretsecretsecretsecretsecr'
+end
+
+before do
+  session[:lists] ||= []
+end
+
 get '/' do
   redirect "/lists"
 end
 
 get '/lists' do
-  @lists = [
-    {name: "Lunch Groceries", todos: []},
-    {name: "Dinner Groceries", todos: []}
-  ]
-  erb :lists, layout: :layout
+  @lists = session[:lists]
+  erb :lists
+end
+
+get '/lists/new' do
+  erb :new_list
+end
+
+post '/lists' do
+  session[:lists] << {name: params[:list_name], todos: []}
+  redirect '/lists'
 end
