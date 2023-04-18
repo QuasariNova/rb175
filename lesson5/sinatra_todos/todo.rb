a7ee0926aad05778f202fcffbe68335bd7055574
Christopher Perkins <Christopher.Perkins@null.net>
2023-04-18 17:45:34 -0400

    L5A18: Added ability to add Todos

--- lesson5/sinatra_todos/todo.rb
+++ lesson5/sinatra_todos/todo.rb
@@ -29,7 +29,7 @@ end
 
 # Verifies if a list_name is valid. If not, it will render render_on_error,
 # otherwise it executes the block passed to it.
-def verify_list_name(list_name, render_on_error)
+def validate_list_name(list_name, render_on_error)
   if !(1..100).cover? list_name.size
     session[:error] = "List name must be between 1 and 100 characters"
   elsif session[:lists].any? { |list| list[:name] == list_name }
@@ -44,7 +44,7 @@ end
 post '/lists' do
   list_name = params[:list_name].strip
 
-  verify_list_name list_name, :new_list do
+  validate_list_name list_name, :new_list do
     session[:lists] << {name: list_name, todos: []}
     session[:success] = "The list has been created."
     redirect '/lists'
@@ -53,7 +53,7 @@ end
 
 # Checks if id is valid. If its not, it redirects, otherwise it runs the block
 # passed to it.
-def verify_id(id)
+def validate_id(id)
   unless (0...session[:lists].size).cover? id
     session[:error] = "List does not exist."
   end
@@ -65,7 +65,7 @@ end
 # Renders a todo list
 get '/lists/:id' do
   id = params[:id].to_i
-  verify_id id do
+  validate_id id do
     @list = session[:lists][id]
     erb :list
   end
@@ -74,7 +74,7 @@ end
 # Edit Existing Todo List
 get '/lists/:id/edit' do
   id = params[:id].to_i
-  verify_id id do
+  validate_id id do
     @list = session[:lists][id]
     erb :edit_list
   end
@@ -84,11 +84,11 @@ end
 post '/lists/:id' do
   id = params[:id].to_i
 
-  verify_id id do
+  validate_id id do
     @list = session[:lists][id]
     list_name = params[:list_name].strip
 
-    verify_list_name list_name, :edit_list do
+    validate_list_name list_name, :edit_list do
       @list[:name] = list_name
       session[:success] = "The list has been updated."
       redirect "/lists/#{id}"
@@ -100,10 +100,38 @@ end
 post '/lists/:id/delete' do
   id = params[:id].to_i
 
-  verify_id id do
+  validate_id id do
     session[:lists].delete_at id
     session[:success] = "The list has been removed."
 
-    redirect "/lists"
+    redirect '/lists'
+  end
+end
+
+def validate_todo_name(list, todo_name)
+  if !(1..100).cover? todo_name.size
+    session[:error] = "Todo name must be between 1 and 100 characters"
+  elsif list[:todos].any? { |todo| todo[:name] == todo_name }
+    session[:error] = "Todo name must be unique"
+  end
+  return erb :list if session[:error]
+
+  yield
+end
+
+# Add Todo to List
+post '/lists/:id/todos' do
+  id = params[:id].to_i
+
+  validate_id id do
+    todo = params[:todo].strip
+    @list = session[:lists][id]
+
+    validate_todo_name @list, todo do
+      @list[:todos] << {name: todo, completed: false}
+      session[:success] = "Todo added to list."
+
+      redirect "/lists/#{id}"
+    end
   end
 end
